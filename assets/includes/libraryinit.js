e4b38a011fb96a510d4176df0923a1b80c5352b0
cocap <mopradk@gmail.com>
2017-08-11 10:42:03 +0200

    D.O.D

--- assets/includes/libraryinit.js
+++ assets/includes/libraryinit.js
@@ -7,6 +7,8 @@ $(document).ready(function(){
 
   $('.carousel').carousel();
 
+	$('.parallax').parallax();
+
   $('.button-collapse').sideNav({
       menuWidth: 200,
       edge: 'left',
@@ -16,6 +18,10 @@ $(document).ready(function(){
       onClose: function(el) {  },
     }
   );
+
+	$('.collapsible li').on('click', function(){
+	  $(this).toggleClass('opened');
+	})
 
   $(function() {
       //caches a jQuery object containing the header element
@@ -93,3 +99,14 @@ $(document).ready(function(){
     $("#locations li:first").trigger("click");
 
   });
+
+
+	/*ANALYTICS*/
+
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-103226387-1', 'auto');
+	  ga('send', 'pageview');
